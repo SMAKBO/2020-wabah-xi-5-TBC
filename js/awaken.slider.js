e0ca01e0d5673e29ccca7b908e99f4f6a0325973
quintanadzarani <59874607+quintanadzarani@users.noreply.github.com>
2020-05-02 21:58:08 +0700

    Delete awaken.slider.js

--- js/awaken.slider.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-  jQuery('.flexslider').flexslider({
-    animation: "fade",
-    direction: "horizontal",
-	slideshowSpeed: 6000,
-	animationSpeed: 600,
-  });
-});
